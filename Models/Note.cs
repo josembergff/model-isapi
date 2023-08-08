a6f0c8e1436e9c6d205a0a24d4542f0ab8c84f2a
Josemberg Ferreira <josembergff@gmail.com>
2023-08-08 08:57:50 -0300

    Update model Note properties

--- Models/Note.cs
+++ Models/Note.cs
@@ -1,6 +1,13 @@
-﻿namespace model_isapi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace model_isapi.Models
 {
     public class Note
     {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
