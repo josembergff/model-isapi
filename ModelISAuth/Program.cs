8a5bc11f931e5005a442e40a2d5492d0f5018492
Josemberg Ferreira <josembergff@gmail.com>
2023-08-08 10:36:31 -0300

    Add configurations identity server

--- ModelISAuth/Program.cs
+++ ModelISAuth/Program.cs
@@ -1,8 +1,18 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
 var builder = WebApplication.CreateBuilder(args);
 
 #region configure services
 
-builder.Services.AddIdentityServer();
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(new List<Client>())
+    .AddInMemoryIdentityResources(new List<IdentityResource>())
+    .AddInMemoryApiResources(new List<ApiResource>())
+    .AddInMemoryApiScopes(new List<ApiScope>())
+    .AddTestUsers(new List<TestUser>())
+    .AddDeveloperSigningCredential()
+    ;
 
 #endregion
 
