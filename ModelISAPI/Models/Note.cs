6f6cac8e04a6a96375c4749df10afdf17fd7f5d9
Josemberg Ferreira <josembergff@gmail.com>
2023-08-08 09:10:31 -0300

    Add new model

---
+++ ModelISAPI/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelISAPI.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+    }
+}
